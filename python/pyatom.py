2a752e80095097a51f12dd84660e49180d6bb00e
Brandon Pierce <brandon@ihashacks.com>
2012-05-24 22:43:43 -0700

    initial commit

---
+++ python/pyatom.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#
+#  20120524 by Brandon Pierce <brandon@ihashacks.com>
+#
+#  Dump the URL of the most recent entry in an atom feed.
+#
+
+import feedparser
+
+feed = feedparser.parse('http://www.ihashacks.com/atom.xml')
+print feed.entries[0].link
